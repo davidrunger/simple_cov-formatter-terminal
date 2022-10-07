27c5dae97a2f5d7b1bf5c4f2f02374091e5136c1
David Runger <davidjrunger@gmail.com>
2022-10-07 12:33:33 -0500

    Fix gemspec todos

--- lib/simple_cov/formatter/terminal.rb
+++ lib/simple_cov/formatter/terminal.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "terminal/version"
+require_relative 'terminal/version'
 
-module SimpleCov
-  module Formatter
-    module Terminal
-      class Error < StandardError; end
-      # Your code goes here...
-    end
-  end
+# Your code goes here...
+class SimpleCov::Formatter::Terminal::Error < StandardError
+  # Your code goes here...
 end
