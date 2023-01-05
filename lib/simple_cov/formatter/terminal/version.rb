b877702ecfd753d578a93a3df115401fece187f8
David Runger <davidjrunger@gmail.com>
2023-01-05 16:38:05 -0600

    Prepare to release v0.1.1

--- lib/simple_cov/formatter/terminal/version.rb
+++ lib/simple_cov/formatter/terminal/version.rb
@@ -5,6 +5,6 @@ module SimpleCov::Formatter; end
 
 # rubocop:disable Style/StaticClass
 class SimpleCov::Formatter::Terminal
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
 # rubocop:enable Style/StaticClass
